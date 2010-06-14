93c702b1e7a8d39241ead747609bff849f6cd82f
Stephan Gabler <stephan.gabler@gmail.com>
2010-06-14 18:04:13 +0200

    a little bit

--- ex2/ex2.py
+++ ex2/ex2.py
@@ -3,3 +3,12 @@
 from pypsignifit import *
 
 execfile("data_june_14_2010.py")
+
+n = 10
+reversals = adaptive_data[diff(adaptive_data)[1:]*diff(adaptive_data)[:-1] == 0]
+
+n_last = adaptive_data[len(adaptive_data)-n:len(adaptive_data),0]
+
+geo_mean = reduce(lambda x, y : x*y, n_last)**(1./n)
+ari_mean = np.sqrt(sum([elem**2 for elem in n_last]))
+print ari_mean
